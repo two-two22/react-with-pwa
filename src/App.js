dfab409c0b33785a3b43d70632fe559eef4f2c72
yes-ee <yes0112@naver.com>
2022-09-01 01:24:25 +0900

    3 업로드 가능하게 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mainpage from "./components/1_mainpage";
 import PctChoice from "./components/3_pct_choice";
 import Picture_2 from "./components/2-1-1_picture";
-import Picture_3 from "./components/3-1_picture";
 import Ai from "./components/3-1-1_ai";
 import Self from "./components/3-2-1_self";
 import Personal_color_result from "./components/Personal_color_result";
@@ -20,13 +15,15 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Mainpage />}/>
-          <Route path="2-1_picture" element={<Picture_2 />} />
+          <Route path="/" element={<Mainpage />} />
+          <Route path="2-1-1_picture" element={<Picture_2 />} />
           <Route path="3_pct_choice" element={<PctChoice />} />
-          <Route path="3-1_picture" element={<Picture_3 />} />
           <Route path="3-1-1_ai" element={<Ai />} />
           <Route path="3-2-1_self" element={<Self />} />
-          <Route path="Personal_color_result" element={<Personal_color_result />} />
+          <Route
+            path="Personal_color_result"
+            element={<Personal_color_result />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
