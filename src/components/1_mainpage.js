cd7f2ecfb2136970c1e288f4cdd75dc5df564b75
heemin728 <idkhm0728@naver.com>
2022-09-02 14:13:18 +0900

    시작 페이지 수정

--- src/components/1_mainpage.js
+++ src/components/1_mainpage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./1_mainpage.css";
+import { AwesomeButton } from "react-awesome-button";
+
 
 function Mainpage() {
   return (
@@ -18,12 +20,18 @@ function Mainpage() {
         <br />
         ME
       </p>
-      <p>
-        <Link to="/2-1-1_picture">나의 얼굴 분석하러 가기</Link>
-      </p>
-      <p>
-        <Link to="/3_pct_choice">퍼스널컬러 분석하러 가기</Link>
-      </p>
+      <div>
+      <Link to="/2-1-1_picture" >
+        <AwesomeButton type="link">나의 얼굴 분석하러 가기</AwesomeButton>
+      </Link>
+      </div><br/>
+      
+      <div>
+      <Link to="/3_pct_choice" >
+            <AwesomeButton type="link">퍼스널컬러 분석하러 가기</AwesomeButton>
+      </Link>
+      </div>
+
     </div>
   );
 }
