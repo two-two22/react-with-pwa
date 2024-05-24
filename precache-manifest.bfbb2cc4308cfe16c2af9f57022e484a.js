e16bfdebee77d6eb6203b7a7982db6d6955ff878
leesiha <mlice1030@soongsil.ac.kr>
2024-05-25 04:36:06 +0900

    Updates

---
+++ precache-manifest.bfbb2cc4308cfe16c2af9f57022e484a.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e3ef2288637b8f21e15",
+    "url": "/react-with-pwa/static/css/main.66ff2b69.chunk.css"
+  },
+  {
+    "revision": "8e3ef2288637b8f21e15",
+    "url": "/react-with-pwa/static/js/main.8e3ef228.chunk.js"
+  },
+  {
+    "revision": "8350b9eb958d55b7e207",
+    "url": "/react-with-pwa/static/js/1.8350b9eb.chunk.js"
+  },
+  {
+    "revision": "227e5df7f3103a9d78d8",
+    "url": "/react-with-pwa/static/css/2.95b22fa9.chunk.css"
+  },
+  {
+    "revision": "227e5df7f3103a9d78d8",
+    "url": "/react-with-pwa/static/js/2.227e5df7.chunk.js"
+  },
+  {
+    "revision": "3b26f4cefd979a97315a",
+    "url": "/react-with-pwa/static/js/runtime~main.3b26f4ce.js"
+  },
+  {
+    "revision": "546549379d9f953d4e39c8fae906a988",
+    "url": "/react-with-pwa/static/media/cos_summer_light.54654937.png"
+  },
+  {
+    "revision": "433173b9615825f3f5eaadda94d94cd1",
+    "url": "/react-with-pwa/static/media/hair_spring.433173b9.png"
+  },
+  {
+    "revision": "74db4eb8615f38b0495ccf8f4e0ebdec",
+    "url": "/react-with-pwa/static/media/hair_summer.74db4eb8.png"
+  },
+  {
+    "revision": "5d04e869eeb72cbb9bfff36012377b55",
+    "url": "/react-with-pwa/static/media/hair_autumn.5d04e869.png"
+  },
+  {
+    "revision": "5cb7add90e8c66bd4c15aa11804782a5",
+    "url": "/react-with-pwa/static/media/hair_winter.5cb7add9.png"
+  },
+  {
+    "revision": "7453ed049e406701a49c8fc083241d9f",
+    "url": "/react-with-pwa/static/media/icon.7453ed04.png"
+  },
+  {
+    "revision": "46f6fdd98a51ee11f576026694bd5c03",
+    "url": "/react-with-pwa/static/media/user.46f6fdd9.png"
+  },
+  {
+    "revision": "6d64694580b802a458db98d074d6c9a6",
+    "url": "/react-with-pwa/static/media/spinner.6d646945.gif"
+  },
+  {
+    "revision": "880c82fa49acb775ae04aef48b692b21",
+    "url": "/react-with-pwa/static/media/faceimage.880c82fa.jpg"
+  },
+  {
+    "revision": "8a6b74b80a80c3a5ac216999e2094db6",
+    "url": "/react-with-pwa/static/media/spring_bright_color.8a6b74b8.png"
+  },
+  {
+    "revision": "0ced24eeb150ae2afe9b8bdcefc8608a",
+    "url": "/react-with-pwa/static/media/spring_light_color.0ced24ee.png"
+  },
+  {
+    "revision": "3f07d2e5122a6d6c671b2fca1c3f1dfe",
+    "url": "/react-with-pwa/static/media/summer_light_color.3f07d2e5.png"
+  },
+  {
+    "revision": "a3d6fe883bfe35d398fa49fc93ce8839",
+    "url": "/react-with-pwa/static/media/summer_mute_color.a3d6fe88.png"
+  },
+  {
+    "revision": "0f926bc754264db2f7df0b82e373ee6f",
+    "url": "/react-with-pwa/static/media/autumn_mute_color.0f926bc7.png"
+  },
+  {
+    "revision": "35ec3ca04607a3c438051c4146a4ba30",
+    "url": "/react-with-pwa/static/media/autumn_deep_color.35ec3ca0.png"
+  },
+  {
+    "revision": "82958302c84603bd1a51b7dd85b33008",
+    "url": "/react-with-pwa/static/media/winter_deep_color.82958302.png"
+  },
+  {
+    "revision": "9200f107c637347ee03a7719338bcad5",
+    "url": "/react-with-pwa/static/media/winter_bright_color.9200f107.png"
+  },
+  {
+    "revision": "08ef42d3e5290e5d6af0ab2b0cc3c4a6",
+    "url": "/react-with-pwa/static/media/cos_spring_light.08ef42d3.png"
+  },
+  {
+    "revision": "173e876dc97738385806f06286937722",
+    "url": "/react-with-pwa/static/media/cos_spring_bright.173e876d.png"
+  },
+  {
+    "revision": "fdbf6db21052de233181a7b74c973ea7",
+    "url": "/react-with-pwa/static/media/cos_autumn_mute.fdbf6db2.png"
+  },
+  {
+    "revision": "3fdaaba07e3daa7e77c5e0f5ddc07387",
+    "url": "/react-with-pwa/static/media/cos_autumn_deep.3fdaaba0.png"
+  },
+  {
+    "revision": "f51d528e147f00464c2db735ddb54bd8",
+    "url": "/react-with-pwa/static/media/cos_winter_deep.f51d528e.png"
+  },
+  {
+    "revision": "de6a7659bd01f2270afd500f8d5d50a7",
+    "url": "/react-with-pwa/static/media/cos_winter_bright.de6a7659.png"
+  },
+  {
+    "revision": "cb1b858942f41ed126aad440c1917ae7",
+    "url": "/react-with-pwa/static/media/star_spring_light.cb1b8589.png"
+  },
+  {
+    "revision": "6ee759435112c2cce0c771726c736ba0",
+    "url": "/react-with-pwa/static/media/star_spring_bright.6ee75943.png"
+  },
+  {
+    "revision": "a384c25e22967a33d7317e0bacb3985a",
+    "url": "/react-with-pwa/static/media/star_summer_light.a384c25e.png"
+  },
+  {
+    "revision": "72053eb90a69bf9313fafb01aff0e9f3",
+    "url": "/react-with-pwa/static/media/star_summer_mute.72053eb9.png"
+  },
+  {
+    "revision": "f717891eb36ff2fd9684fe14fef359be",
+    "url": "/react-with-pwa/static/media/star_autumn_mute.f717891e.png"
+  },
+  {
+    "revision": "72b8940dcb6359974a24a10086ddab48",
+    "url": "/react-with-pwa/static/media/star_autumn_deep.72b8940d.png"
+  },
+  {
+    "revision": "e9da2f6452142f3db130f3505bedc952",
+    "url": "/react-with-pwa/static/media/star_winter_deep.e9da2f64.png"
+  },
+  {
+    "revision": "3f24475ed9474b0ace06627fdb5f5135",
+    "url": "/react-with-pwa/static/media/star_winter_bright.3f24475e.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-with-pwa/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "210989664066c01d8ffdbdf56bb773cd",
+    "url": "/react-with-pwa/static/media/NotoSansKR-Regular.21098966.otf"
+  },
+  {
+    "revision": "be3c4a43db1bfe054eb4b58ea6dbece8",
+    "url": "/react-with-pwa/static/media/Vogue.be3c4a43.ttf"
+  },
+  {
+    "revision": "e914a10a1bd0088fb8f743fc7569749f",
+    "url": "/react-with-pwa/static/media/NotoSansKR-Light.e914a10a.otf"
+  },
+  {
+    "revision": "385425ec0fe7923f12c16ecae5a6aa14",
+    "url": "/react-with-pwa/static/media/output_spring.385425ec.png"
+  },
+  {
+    "revision": "9d88b8f302ea7450265618465dc2a355",
+    "url": "/react-with-pwa/static/media/output_summer.9d88b8f3.png"
+  },
+  {
+    "revision": "e63c2d69dc850a595b146bf050c8274e",
+    "url": "/react-with-pwa/static/media/output_autumn.e63c2d69.png"
+  },
+  {
+    "revision": "0b250458aac36c4c575ea44b12bc57a9",
+    "url": "/react-with-pwa/static/media/output_winter.0b250458.png"
+  },
+  {
+    "revision": "421a52ea8bd5371ab02cb8317a05aa7c",
+    "url": "/react-with-pwa/static/media/output_spring_bright.421a52ea.png"
+  },
+  {
+    "revision": "df079c60275d3abb6f99912910d45d5b",
+    "url": "/react-with-pwa/static/media/output_spring_light.df079c60.png"
+  },
+  {
+    "revision": "11471e61d5f4dce933e4e8a13b49ffee",
+    "url": "/react-with-pwa/static/media/output_summer_light.11471e61.png"
+  },
+  {
+    "revision": "bf03b7bae151152c4000e2d11aedbd7b",
+    "url": "/react-with-pwa/static/media/output_summer_mute.bf03b7ba.png"
+  },
+  {
+    "revision": "1bb6879e29e330ca9a2bf56008c8091e",
+    "url": "/react-with-pwa/static/media/output_autumn_deep.1bb6879e.png"
+  },
+  {
+    "revision": "383c304488d38f6b93d87c5e0f6303ac",
+    "url": "/react-with-pwa/static/media/output_winter_bright.383c3044.png"
+  },
+  {
+    "revision": "764566c90588305cd6ead1589d6b7105",
+    "url": "/react-with-pwa/static/media/output_autumn_mute.764566c9.png"
+  },
+  {
+    "revision": "80bffcc4dad8c331fe281eb2935d757c",
+    "url": "/react-with-pwa/static/media/output_winter_deep.80bffcc4.png"
+  },
+  {
+    "revision": "e219f6c803762e07260f7c8ede30efbe",
+    "url": "/react-with-pwa/index.html"
+  }
+];
